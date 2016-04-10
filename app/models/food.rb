15c03b82a945ae089b726ac9e425330f1985f48f
Corey Hinshaw <hinshaw.25@osu.edu>
2016-04-10 12:13:07 -0400

    Food names should be unique

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,5 +2,5 @@ class Food < ActiveRecord::Base
   has_many :ingredients, inverse_of: :food, dependent: :destroy
   has_many :recipes, through: :ingredients
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
