345eb0280f99ccdc719f1ed495a8715f042a6b47
Corey Hinshaw <hinshaw.25@osu.edu>
2016-12-22 12:08:00 -0500

    Use separate permitted parameters for update and create in recipe policy

--- app/policies/recipe_policy.rb
+++ app/policies/recipe_policy.rb
@@ -15,9 +15,13 @@ class RecipePolicy < ApplicationPolicy
     change_owner?
   end
 
-  def permitted_attributes
-    attributes = [:name, :description, :active_time, :total_time, :yield,
-      :equipment, :image, :delete_image, user_ids: []]
+  def permitted_attributes_for_create
+    [:name, :description, :active_time, :total_time, :yield, :equipment, :image,
+      :delete_image, user_ids: []]
+  end
+
+  def permitted_attributes_for_update
+    attributes = permitted_attributes_for_create
     attributes << :owner_id if change_owner?
     attributes
   end
